f30862f59540334b8f05dfe5c050ce660f27062e
David Acosta <david.acosta.ing@gmail.com>
2025-08-20 12:24:33 -0500

    Loading a texture and placing it in a sprite as background

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,34 @@
 #include <optional>
 #include <print>
 #include "SFML/Graphics.hpp"
-#include "SFML/Window/WindowEnums.hpp"
+
 
 
 int main(){
     sf::VideoMode vm({1920, 1080});
     sf::RenderWindow window(vm, "Timber", sf::Style::Default);
+
+    const sf::Texture textureBackground("assets/textures/background.png");
+    sf::Sprite spriteBackground(textureBackground);
+    spriteBackground.setPosition({0,0});
+
     while(window.isOpen()){
-        std::print("the window just opened\n");
         while (const std::optional<sf::Event> event = window.pollEvent()) {
             // Close window then exit
             if(event->is<sf::Event::Closed>()){
                 window.close();
             }
         }
+
+        // Handle the players input
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)){
+            window.close();
+        }
+
+        // Update the scene
         window.clear();
+        // Draw the scene
+        window.draw(spriteBackground);
         window.display();
     }
     return 0;
