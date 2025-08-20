c777e4220e654e58216259763b1404ee7a13c91f
David Acosta <david.acosta.ing@gmail.com>
2025-08-20 11:12:45 -0500

    Creating a bash for Windows and unix like systems

--- build_run.sh
+++ build_run.sh
@@ -1,12 +1,34 @@
 set -e #Exit error
 
 BUILD_DIR="build"
+EXECUTABLE="Timber"
+
+UNAME_OUT="$(uname -s)"
+case "${UNAME_OUT}" in
+    Linux*|Darwin*)
+        PLATFORM="unix"
+        GENERATOR="Unix Makefiles"
+        EXECUTABLE="./$BUILD_DIR/$EXECUTABLE"
+        ;;
+    MINGW* |MSYS*|CYGWIN*)
+        PLATFORM="windows"
+        GENERATOR="Ninja"
+        EXECUTABLE="./$BUILD_DIR/$EXECUTABLE.exe"
+        ;;
+    *)
+        echo "Unsupported platform: ${UNAME_OUT}"
+        exit 1
+        ;;
+esac
+
+echo "Detected platform: $PLATFORM"
+echo "Using generator: $GENERATOR"
+
 mkdir -p "$BUILD_DIR"
 
-
-cmake -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=DEBUG
-
+cmake -S . -B "$BUILD_DIR" -G "$GENERATOR" -DCMAKE_BUILD_TYPE=DEBUG
 
 cmake --build "$BUILD_DIR" --parallel
 
-"$BUILD_DIR/Timber"
+echo "Running: $EXECUTABLE"
+"$EXECUTABLE"
