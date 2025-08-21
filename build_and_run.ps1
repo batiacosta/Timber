c0ded2fc93a840a1fe03351d7d503bacafa2cad1
David Acosta <david.acosta.ing@gmail.com>
2025-08-20 20:06:55 -0500

    Using MSVC for Windows

--- build_and_run.ps1
+++ build_and_run.ps1
@@ -7,11 +7,16 @@ if (!(Test-Path $buildDir)) {
     New-Item -ItemType Directory -Path $buildDir | Out-Null
 }
 
-# Configure with CMake
-cmake -S . -B $buildDir -G "Ninja" -DCMAKE_BUILD_TYPE=Debug
+# Configure with CMake using Visual Studio generator
+cmake -S . -B $buildDir -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Debug
 
-# Build the project
-cmake --build $buildDir --parallel
+# Build the project (Debug configuration)
+cmake --build $buildDir --config Debug
 
 # Run the executable
-Start-Process "$buildDir\Timber.exe"
+$exePath = Join-Path $buildDir "Debug\Timber.exe"
+if (Test-Path $exePath) {
+    Start-Process $exePath
+} else {
+    Write-Host "Executable not found: $exePath"
+}
